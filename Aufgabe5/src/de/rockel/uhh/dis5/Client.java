e5e1242cc49c208e35354e758a02b72a08f4c922
Sebastian Rockel <sebastianrockel@gmail.com>
2010-06-07 19:15:01 +0200

    Added code base for Aufgabe5

---
+++ Aufgabe5/src/de/rockel/uhh/dis5/Client.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * based on template by
+ *     Markus Alexander Kuppe (ecf-dev_eclipse.org <at> lemmster <dot> de) - initial API and implementation
+ ******************************************************************************/
+package de.rockel.uhh.dis5;
+
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Client implements Runnable {
+
+	private Random random;
+	private int id;
+	private int transactionCycles;
+	private CyclicBarrier barrier;
+	private PersistenceManager persistenceManager;
+
+	public Client(PersistenceManager pm, int anID, int cycles, CyclicBarrier aBarrier) {
+		persistenceManager = pm;
+		id = anID;
+		transactionCycles = cycles;
+		barrier = aBarrier;
+		random = new Random();
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		// wait until all others threads are ready
+		try {
+			barrier.await();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+			return;
+		} catch (BrokenBarrierException e1) {
+			e1.printStackTrace();
+			return;
+		}
+		
+		String data = getClass().getName() + id;
+		for (int i = 0; i < transactionCycles; i++) {
+			long aTransactionID = persistenceManager.beginTransaction();
+			
+			int writeCycles = random.nextInt(31);
+			for(int j = 0; j < writeCycles; j++) {
+				//calculate the page id correctly so each client gets its own page range
+				long pageId = (id * 10) + random.nextInt(10);
+				persistenceManager.write(aTransactionID, pageId, data + " at " + System.currentTimeMillis());
+				sleepRandom();
+			}
+			persistenceManager.commit(aTransactionID);
+			sleepRandom();
+		}
+		
+		System.out.println("Client " + id + " is done!");
+	}
+	
+	// wait for a while before we start the next TA 
+	private void sleepRandom() {
+		try {
+			Thread.sleep(random.nextInt(1000));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+}
---
+++ Aufgabe5/src/de/rockel/uhh/dis5/Log.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * based on template by
+ *     Markus Alexander Kuppe (ecf-dev_eclipse.org <at> lemmster <dot> de) - initial API and implementation
+ ******************************************************************************/
+package de.rockel.uhh.dis5;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import de.rockel.uhh.dis5.LogRecord.LogType;
+
+public class Log {
+
+	public static final String LOG = "LOG";
+	public static final String PREFIX = System.getProperty("basePath") + File.separator + "log" + File.separator;
+
+	private FileWriter fw;
+	
+	public Log() {
+		File path = new File(PREFIX);
+		path.mkdirs();
+		
+		File file = new File(PREFIX + LOG);
+		try {
+			fw = new FileWriter(file, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void writePage(long aLSN, long aTransactionId, long aPageId, String data) {
+		try {
+			fw.write(new LogRecord(LogType.PAGE, aLSN, aTransactionId, aPageId, data).toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeBOT(long aLSN, long aTransactionId) {
+		try {
+			fw.write(new LogRecord(LogType.BOT, aLSN, aTransactionId).toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeEOT(long aLSN, long aTransactionId) {
+		try {
+			fw.write(new LogRecord(LogType.EOT, aLSN, aTransactionId).toString());
+			fw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
